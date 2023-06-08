ba87e8060270cf23e958f125333e615c7d9d0238
shonsky <shon.kzidenov@gmail.com>
2023-06-08 22:21:22 +0600

    modified results

--- controllers/exam.py
+++ controllers/exam.py
@@ -1,5 +1,5 @@
 from aiogram import Dispatcher, types
-from main import test, bot
+from main import test, bot, dp
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup
@@ -8,7 +8,8 @@ from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup
 
 class QState(StatesGroup):
     Answered = State() 
-    answer = ''
+    Finished = State()
+    answers = []
     questionNumber = 0
     result = 0
 
@@ -20,6 +21,7 @@ async def startTest(message: Message, state: FSMContext):
         one_time_keyboard=True,
          ).row(buttonOk, buttonNo)
     QState.result = 0
+    QState.answers = []
     await state.set_state(QState.Answered)
     await bot.send_message(message.from_user.id, "Let's get started! Press 'OK'.", reply_markup=startOptions)
     # questions = test.find()
@@ -49,17 +51,26 @@ async def Test(message: Message, state: FSMContext):
             one_time_keyboard=True,
              ).row(button1, button2, button3)
         QState.questionNumber += 1
-        if QState.answer == message.text:
-            QState.result += 1
-        QState.answer = question['answer']
+        QState.answers.append(message.text)
         await state.set_state(QState.Answered)
         await bot.send_message(message.from_user.id, testQuestion, reply_markup=options)
     else:
-        if QState.answer == message.text:
-            QState.result += 1
-        await bot.send_message(message.from_user.id, "No more questions. Ur score is: " + str(QState.result))
+        await bot.send_message(message.from_user.id, "No more questions. Tap to /showresults")
+        QState.answers.append(message.text)
         QState.questionNumber = 0
-        await state.finish()
+        await state.set_state(QState.Finished)
+        # await state.finish()
+
+async def showResults(message: Message, state: FSMContext):
+    for i in range(len(QState.answers)-1):
+        if QState.answers[i+1] == test.find()[i]['answer']:
+            QState.result += 1
+    await state.finish()
+    await bot.send_message(message.from_user.id, "⬇️ This dude got " + str(QState.result) + " right answers out of " + str(len(QState.answers)-1))
+    profilePictures = await dp.bot.get_user_profile_photos(message.from_user.id)
+    await bot.send_photo(message.from_user.id, dict((profilePictures.photos[0][0])).get("file_id"))
+
 def initTest(dp: Dispatcher):
     dp.register_message_handler(startTest, content_types=['text'], state='*', commands=['testme'])
     dp.register_message_handler(Test, content_types=['text'], state = QState.Answered)
+    dp.register_message_handler(showResults, content_types=['text'], state= QState.Finished, commands=['showresults'])
