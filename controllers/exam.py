35df84175f3dea8264cc80fb9ae9ae101a66ef53
shonsky <shon.kzidenov@gmail.com>
2023-06-08 21:09:49 +0600

    added results

--- controllers/exam.py
+++ controllers/exam.py
@@ -2,17 +2,26 @@ from aiogram import Dispatcher, types
 from main import test, bot
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup
 
 
 
 class QState(StatesGroup):
     Answered = State() 
+    answer = ''
     questionNumber = 0
     result = 0
 
-async def startTest(message: types.Message, state: FSMContext):
-    await bot.send_message(message.from_user.id, "Let's get started! Press 'OK'.")
+async def startTest(message: Message, state: FSMContext):
+    buttonOk = KeyboardButton('OK')
+    buttonNo = KeyboardButton('No')
+    startOptions = ReplyKeyboardMarkup(
+        resize_keyboard=True,
+        one_time_keyboard=True,
+         ).row(buttonOk, buttonNo)
+    QState.result = 0
     await state.set_state(QState.Answered)
+    await bot.send_message(message.from_user.id, "Let's get started! Press 'OK'.", reply_markup=startOptions)
     # questions = test.find()
     # for el in questions:
     #     testQuestion = (
@@ -22,21 +31,33 @@ async def startTest(message: types.Message, state: FSMContext):
     #         + '\n 3) ' + el['variants'][2]
     #         )
     #     await bot.send_message(message.from_user.id, testQuestion)
-async def Test(message: types.Message, state: FSMContext):
+async def Test(message: Message, state: FSMContext):
     questions = test.find()
     if QState.questionNumber < len(list(test.find())):
         question = questions[QState.questionNumber]
         testQuestion = (
                 question['question'] 
-                + '\n 1) ' + question['variants'][0] 
-                + '\n 2) ' + question['variants'][1] 
-                + '\n 3) ' + question['variants'][2]
+                # + '\n 1) ' + question['variants'][0] 
+                # + '\n 2) ' + question['variants'][0] 
+                # + '\n 3) ' + question['variants'][2]
                 )
+        button1 = KeyboardButton(question['variants'][0])
+        button2 = KeyboardButton(question['variants'][1])
+        button3 = KeyboardButton(question['variants'][2])
+        options = ReplyKeyboardMarkup(
+            resize_keyboard=True,
+            one_time_keyboard=True,
+             ).row(button1, button2, button3)
         QState.questionNumber += 1
+        if QState.answer == message.text:
+            QState.result += 1
+        QState.answer = question['answer']
         await state.set_state(QState.Answered)
-        await bot.send_message(message.from_user.id, testQuestion)
+        await bot.send_message(message.from_user.id, testQuestion, reply_markup=options)
     else:
-        await bot.send_message(message.from_user.id, "no more questions")
+        if QState.answer == message.text:
+            QState.result += 1
+        await bot.send_message(message.from_user.id, "No more questions. Ur score is: " + str(QState.result))
         QState.questionNumber = 0
         await state.finish()
 def initTest(dp: Dispatcher):
--- controllers/start.py
+++ controllers/start.py
@@ -1,7 +1,8 @@
 from aiogram import Dispatcher, types
 
 async def start(message: types.Message):
-    message_text = """Write /game to play a game"""
+    message_text = """Write /game to play a game (currenly disabled)
+    Write /testme to test yourself!"""
     await message.reply(message_text)
 
 def setup(dp: Dispatcher):
